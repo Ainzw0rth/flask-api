a23107757619f9c5defc699b264a8e3ac21cade6
louis <louiscaesapdf@gmail.com>
2024-07-19 13:18:37 +0700

    add more routing and env

--- api/index.py
+++ api/index.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+from utils.model import generate_response
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def hello():
+    return "Hello, World!"
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/test')
+def hello():
+    return "test"
+
+@app.route('/whatsapp', methods=['GET','POST'])
+def whatsapp():
+    response = MessagingResponse()
+
+    # sender = request.form.get('From')  # Sender's phone number
+    message_body = request.form.get('Body')  # User's message
+
+    response.message(generate_response(message_body)) # template
+
+    return str(response)
+
+if __name__ == '__main__':
+    app.run(port=8080)
